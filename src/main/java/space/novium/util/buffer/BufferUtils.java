3764c98e3b280edfafcee896f1a3edf267a99741
Novium <nova@novium.space>
2024-05-26 17:11:27 -0600

    Matrix4f things

--- src/main/java/space/novium/util/buffer/BufferUtils.java
+++ src/main/java/space/novium/util/buffer/BufferUtils.java
@@ -2,6 +2,7 @@ package space.novium.util.buffer;
 
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
+import java.nio.FloatBuffer;
 import java.nio.IntBuffer;
 
 public final class BufferUtils {
@@ -9,6 +10,12 @@ public final class BufferUtils {
     
     public static IntBuffer createIntBuffer(int[] arr){
         IntBuffer result = ByteBuffer.allocateDirect(arr.length << 2).order(ByteOrder.nativeOrder()).asIntBuffer();
+        result.put(arr).flip();
+        return result;
+    }
+    
+    public static FloatBuffer createFloatBuffer(float[] arr){
+        FloatBuffer result = ByteBuffer.allocateDirect(arr.length << 2).order(ByteOrder.nativeOrder()).asFloatBuffer();
         result.put(arr).flip();
         return result;
     }
--- src/main/java/space/novium/util/math/Matrix4f.java
+++ src/main/java/space/novium/util/math/Matrix4f.java
@@ -1,8 +1,13 @@
 package space.novium.util.math;
 
+import space.novium.util.buffer.BufferUtils;
+import space.novium.util.math.vector.Vector3f;
+
+import java.nio.FloatBuffer;
+
 public class Matrix4f {
     public static final int SIZE = 4;
-    private float[] elements = new float[SIZE * SIZE];
+    protected float[] elements = new float[SIZE * SIZE];
     
     private Matrix4f(){}
     
@@ -29,5 +34,38 @@ public class Matrix4f {
     
     public static Matrix4f standardOrthographic(){
         return orthographic(-1.0f, 1.0f, -1.0f, 1.0f, -1.0f, 1.0f);
+    }
+    
+    public Matrix4f translate(Vector3f vec){
+        elements[12] += vec.x;
+        elements[13] += vec.y;
+        elements[14] += vec.z;
+        return this;
+    }
+    
+    public Matrix4f scale(float s){
+        return scale(s, s);
+    }
+    
+    public Matrix4f scale(float x, float y){
+        elements[0] = x;
+        elements[5] = y;
+        return this;
+    }
+    
+    public Matrix4f rotate(float degrees){
+        float rads = (float)Math.toRadians(degrees);
+        float sin = (float)Math.sin(rads);
+        float cos = (float)Math.cos(rads);
+        
+        elements[0] = cos;
+        elements[1] = sin;
+        elements[4] = -sin;
+        elements[5] = cos;
+        return this;
+    }
+    
+    public FloatBuffer toFloatBuffer(){
+        return BufferUtils.createFloatBuffer(elements);
     }
 }
