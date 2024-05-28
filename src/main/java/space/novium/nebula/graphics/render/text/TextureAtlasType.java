b130aebc2ed930ce7b4394bd52559feb5aae5b84
Novium <nova@novium.space>
2024-05-27 20:06:49 -0600

    TextureAtlasType
    Easy way for me to add different textures to this

---
+++ src/main/java/space/novium/nebula/graphics/render/text/TextureAtlasType.java
@@ -0,0 +1,34 @@
+package space.novium.nebula.graphics.render.text;
+
+import static org.lwjgl.opengl.GL13.*;
+
+public enum TextureAtlasType {
+    BLANK("blank"),
+    BACKGROUND("background"),
+    ENTITY("entities"),
+    ITEMS("items"),
+    NONE("no_atlas"),
+    TEXT("text"),
+    TILES("tiles"),
+    UI("user_interface");
+    
+    private final String description;
+    private final int glTexture;
+    
+    TextureAtlasType(String description){
+        this.description = description;
+        this.glTexture = GL_TEXTURE0 + this.ordinal(); //This should automatically assign a unique GL_TEXTURE to each atlas type
+    }
+    
+    public String getDescription(){
+        return description;
+    }
+    
+    public int getGlTexture(){
+        return glTexture;
+    }
+    
+    public int getId(){
+        return glTexture - GL_TEXTURE0;
+    }
+}
