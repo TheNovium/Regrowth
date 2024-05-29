6fc13141d970ea29481962ae2ccadc568b7b9420
Novium <nova@novium.space>
2024-05-29 17:13:13 -0600

    Direction enum

---
+++ src/main/java/space/novium/world/data/Direction.java
@@ -0,0 +1,30 @@
+package space.novium.world.data;
+
+import space.novium.impl.annotation.Stringable;
+
+@Stringable(defaultEntry = "SOUTH")
+public enum Direction {
+    NORTH("North"),
+    EAST("East"),
+    SOUTH("South"),
+    WEST("West");
+    
+    private final String value;
+    
+    Direction(String value){
+        this.value = value;
+    }
+    
+    public String getValue() {
+        return value;
+    }
+    
+    public static Direction fromString(String text){
+        for(Direction d : Direction.values()){
+            if(d.value.equalsIgnoreCase(text)){
+                return d;
+            }
+        }
+        return Direction.valueOf(Direction.class.getAnnotation(Stringable.class).defaultEntry());
+    }
+}
