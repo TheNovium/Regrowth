5928956da23ae1696b99fdd537d0a02494ca3a57
Novium <nova@novium.space>
2024-05-29 18:15:14 -0600

    Oops
    I forgot to add the namespace

--- src/main/java/space/novium/Regrowth.java
+++ src/main/java/space/novium/Regrowth.java
@@ -3,6 +3,8 @@ package space.novium;
 import space.novium.nebula.Window;
 
 public class Regrowth {
+    public static final String NAMESPACE = "regrowth";
+    
     private static Window window;
     
     public static void main(String[] args) {
